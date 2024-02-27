147ceafbd20f7a2e610d5f363c16731e2fae3a2f
lasermtv07 <laserman.minecraft@seznam.cz>
2024-02-27 19:10:16 +0100

    added function stuff with stack

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #define PROGSIZE 10240
 
 struct fn {
@@ -24,7 +25,7 @@ int fnLastElement(struct fn fun[PROGSIZE]){
 	}
 	return l;
 }
-
+//gets substring in defined interval
 char* getSubstr(char str[PROGSIZE], int s, int e){
 	char* o=malloc(PROGSIZE); o[0]=0;
 	for(;s<=e;s++){
@@ -32,19 +33,51 @@ char* getSubstr(char str[PROGSIZE], int s, int e){
 	}
 	return o;
 }
+//last non zero.. INDEX, the name would be too long
+int getLastNonZeroInt(int arr[PROGSIZE]){
+	int o=0;
+	for(int i=0;i<PROGSIZE;i++){
+		if(arr[i]!=0) o=i;
+	}
+	return o;
+}
+//'bloat' being considered all other 'f's and 'c's
+char* removeAllInlineBloat(char str[PROGSIZE]){
+	char* w=malloc(PROGSIZE); w[0]=0;
+	strcpy(w,getSubstr(str,1,strlen(str)-1)); //do i have to do this??
+	int lc=0;
+	for(int i=0;i<PROGSIZE;i++){
+		if(w[i]=='c') lc=i;
+	}
+	int ff=false;
+	char*t=malloc(PROGSIZE); t[0]=0; //uhhh...
+	for(int i=0;i<PROGSIZE;i++){
+		if(w[i]=='f') ff=true;
+		if(!ff) strcat(t,stringify(w[i]));
+		if(i==lc) ff=false;
+	}
+
+	return t;
+
+}
 int main(int argc, char**argv){
 	char code[PROGSIZE]; code[0]=0;
-	strcpy(code,argv[1]);
-	
+	//pads the array by one space for.. reasons
+	strcat(code," ");
+	strcat(code,argv[1]);
 	struct fn fun[PROGSIZE];
-	printf("%s",getSubstr(code,3,5));
-	char temp[PROGSIZE]; temp[0]=0;
+	static int stack[PROGSIZE];
 	int acc=0;
 	for(int i=0;i<=strlen(code);i++){
 		if(acc==256 || acc==-1) acc=0;
 		if(code[i]=='i') acc++;
 		if(code[i]=='d') acc--;
 		if(code[i]=='s') acc*=acc;
+		if(code[i]=='f') stack[getLastNonZeroInt(stack)+1]=i;
+		if(code[i]=='c'){
+			printf("%s\n",removeAllInlineBloat(getSubstr(code,stack[getLastNonZeroInt(stack)],i)));
+			stack[getLastNonZeroInt(stack)]=0;
+		}
 
 	}
 	return 0;
